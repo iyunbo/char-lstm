b814a17688d903f53721cef57cffe5bcdc3fc3bc
iyunbo <yunbo.wang@solven.eu>
2019-06-15 19:47:48 +0200

    feat: script for running the entire process

--- run.py
+++ run.py
@@ -1,3 +1,17 @@
-from lstm import preprocessing as prep
+import logging as log
 
-prep.load_data()
+from lstm import preprocessing as prep
+from lstm.model import CharLSTM, train
+
+
+def main():
+    text = prep.load_data()
+    network = CharLSTM(text, n_hidden=256, n_layers=2)
+    train(network, epochs=20, batch_size=15, print_every=300)
+    generated_text = network.generate(1500)
+
+    log.info(generated_text)
+
+
+if __name__ == "__main__":
+    main()
